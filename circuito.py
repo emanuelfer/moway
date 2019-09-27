7f29efa6c4c4db89574d8f5599818f2cf1ae729a
Emanuel Lindoso Ferreira <emanuelstrato999@gmail.com>
2019-09-27 09:34:04 -0300

    Add files via upload

--- circuito.py
+++ circuito.py
@@ -14,7 +14,7 @@ if ret == 0:
     print ('Moway RFUSB Connected')
 else:
     print ('Moway RFUSB not connected. Exit')
-    exit(-1)
+    #exit(-1)
 
 while moway.get_obs_center_left() < 20 and moway.get_obs_center_right() < 20:
     if(moway.get_line_left() > 50): #verifica se o moway ainda está na linha preta
@@ -22,7 +22,7 @@ while moway.get_obs_center_left() < 20 and moway.get_obs_center_right() < 20:
         moway.set_speed(100)
         moway.command_moway(CMD_GO,0)
         moway.wait_mot_end(0)
-    elif moway.get_line_left() <= 50 and moway.get_line_left() > 20: #verifica se chegou em uma bifurcação
+    else: #verifica se chegou em uma bifurcação
         moway.set_distance(10)
         moway.set_speed(100)
         moway.command_moway(CMD_GO,0)
@@ -36,22 +36,24 @@ while moway.get_obs_center_left() < 20 and moway.get_obs_center_right() < 20:
         if (moway.get_line_left() < 50):
             moway.set_rotation(90)
             moway.set_rotation_axis(CENTER)
-            moway.command_moway(CMD_ROTATElEFT,0)
+            moway.command_moway(CMD_ROTATELEFT,0)
             moway.wait_mot_end(0)
 
             moway.set_distance(20)
             moway.set_speed(100)
             moway.command_moway(CMD_GO,0)
             moway.wait_mot_end(0)
-    else: #verifica se chegou no fim de uma linha preta e tem que voltar
-        moway.set_rotation(180)
-        moway.set_rotation_axis(CENTER)
-        moway.command_moway(CMD_ROTATELEFT,0)
-        moway.wait_mot_end(0)
+            
+        if (moway.get_line_left() < 50):
+            moway.set_rotation(180)
+            moway.set_rotation_axis(CENTER)
+            moway.command_moway(CMD_ROTATELEFT,0)
+            moway.wait_mot_end(0)
 
-        moway.set_distance(15)
-        moway.set_speed(100)
-        moway.command_moway(CMD_GO,0)
-        moway.wait_mot_end(0)
+            moway.set_distance(20)
+            moway.set_speed(100)
+            moway.command_moway(CMD_GO,0)
+            moway.wait_mot_end(0)
+   
 
 moway.command_moway(CMD_STOP, 0)
